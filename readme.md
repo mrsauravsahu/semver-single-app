306ea465a6782e42960037314ae1c58d3c217f82
Saurav Sahu <mrsauravsahu@outlook.com>
2022-08-21 13:27:38 +0530

    Release/0.2.0 (#13)
    * feat: initial app (#1)
    
    * fix: remove extra slash from prefix in SINGLE_APP repos (#2)
    
    * feat: create npm project (#3)
    
    * improv: increase prerelease based on commit counting previous tag of same version (#4)
    
    * docs: add author details in code (#5)
    
    * docs: add author details in code
    
    * docs: add root readme
    
    * docs: add desc
    
    * Fix: prerelease count (#6)
    
    * fix: create tag with calculated prerelease number
    
    * docs: add index.js desc
    
    * fix: git tag - prefix with MajorMinorPatch (#7)
    
    * fix: git tag - prefix with MajorMinorPatch
    
    * improv: print version
    
    * Fix: git tag majorminorpatch prefix var (#8)
    
    * fix: service without prefix var for SINGLE_APP repos
    
    * tmp: change in index.js to check version bump
    
    * fix: git tag format (#9)
    
    * fix: git tag format
    
    * tmp: test app version bump
    
    * fix: missing matching parenthesis (#10)
    
    * fix: missing matching parenthesis
    
    * tmp: test app version bump
    
    * docs: add author in readme (#11)
    
    * feat: format code (#12)

---
+++ readme.md
@@ -0,0 +1,5 @@
+# semver-single-app
+
+repo to demonstrate versioning single app repos with Monotools/semver-yeasy.
+
+\- Saurav Sahu
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Author: Saurav Sahu <mrsauravsahu@outlook.com>
+// semver-single-app
+
+const package = require('./package.json')
+
+console.log(`semver-single-app/v${package.version}`);
